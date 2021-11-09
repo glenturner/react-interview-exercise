a4bf066b0d16512d2df8ff40ffd7a6d4c0bf1cdb
glenturner <gturner0316@yahoo.com>
2021-11-08 21:05:37 -0500

    Quick commit by Glen Turner

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent } from "react";
 import { Flex } from "..";
 import { SearchIcon } from "src/Assets";
 import style from "./style.module.scss";
